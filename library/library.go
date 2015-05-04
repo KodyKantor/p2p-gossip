a8fc49c5e270e90dcd185e51eb961148c9a528ad
Kody Kantor <kody.kantor@gmail.com>
2015-05-04 17:43:17 -0500

    Add ability to load resources from file list, and assign unique IDs

--- library/library.go
+++ library/library.go
@@ -6,9 +6,10 @@ package library
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
+	"io/ioutil"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/kodykantor/p2p-gossip/id"
 )
 
 func init() {
@@ -17,28 +18,59 @@ func init() {
 
 //CreateResourceFromJSON take a string representation of json data, and creates
 // a Resource from it.
-func CreateResourceFromJSON(jsn string) (*Resource, error) {
-	if len(jsn) == 0 {
-		return &Resource{}, fmt.Errorf("Json string is zero-length")
+func CreateResourceFromJSON(buf []byte) (*Resource, error) {
+	if buf == nil {
+		return &Resource{}, fmt.Errorf("Json buffer is nil")
 	}
+	if len(buf) == 0 {
+		return &Resource{}, fmt.Errorf("Json buffer is zero-length")
+	}
+
 	dest := new(Resource)
-	reader := strings.NewReader(jsn)
-	buf := make([]byte, len(jsn))
 
-	_, err := reader.Read(buf)
-	if err != nil {
-		return &Resource{}, fmt.Errorf("Error reading string into byte slice: %v", err)
-	}
-
-	err = json.Unmarshal(buf, dest)
+	err := json.Unmarshal(buf, dest)
 	if err != nil {
 		return &Resource{}, fmt.Errorf("Error unmarshalling JSON: %v", err)
 	}
+
 	return dest, nil
 }
 
 //LoadResourcesFrom takes a path to a file that contains JSON descriptions
 //of resources. This function will create a slice of pointers to resources.
-//func LoadResourcesFrom(path string) ([]*Resource, error) {
+func LoadResourcesFrom(path string) ([]*Resource, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading file: %v", err)
+	}
+	logrus.Debugln("File said:", string(data))
 
-//}
+	var dest []*Resource
+	err = json.Unmarshal(data, &dest) //convert bytes to a slice of structs
+	if err != nil {
+		return nil, fmt.Errorf("Error unmarshalling json: %v", err)
+	}
+	logrus.Debugln("JSON is:", string(data))
+
+	return dest, nil
+}
+
+//AssignRandomIDs takes a slice of resources, and assigns each a random ID.
+func AssignRandomIDs(slice []*Resource) error {
+	if slice == nil {
+		return fmt.Errorf("Provided slice is nil")
+	}
+	if len(slice) == 0 {
+		return fmt.Errorf("Provided slice is zero-length")
+	}
+	myid := id.NewID()
+	ch := make(chan id.ID, 1)
+
+	go myid.ServeIDs(ch) //start serving IDs in the background
+	for _, val := range slice {
+		val.resourceID = <-ch //read from the channel, assign to resource
+		logrus.Debugln("Applied random ID to resource: ", val)
+	}
+
+	return nil
+}
--- library/library_test.go
+++ library/library_test.go
@@ -2,7 +2,11 @@ package library
 
 import (
 	"bytes"
+	"io/ioutil"
+	"os"
 	"testing"
+
+	"github.com/Sirupsen/logrus"
 )
 
 //Create a file with some json in it, have the function read it, then
@@ -13,19 +17,73 @@ func TestCreateResourceFromJSON(t *testing.T) {
 	buf.WriteString("{ \"name\": \"cats.jpg\", \"description\": \"cat picture of cats\", ")
 	buf.WriteString("\"location\": \"/tmp/somepath\", \"mimetype\": \"image/jpeg\"}")
 
-	str := buf.String()
-	t.Logf("Testing the json string: %v", str)
+	buffer := buf.Bytes()
+	//t.Logf("Testing the json buffer: %v", buffer)
 
-	result, err := CreateResourceFromJSON(str)
+	result, err := CreateResourceFromJSON(buffer)
 	if err != nil {
 		t.Errorf("Error creating resource from JSON string: %v", err)
 	}
 
 	t.Logf("Resource was decoded as:\n %v", result)
 
-	str = ""
-	_, err = CreateResourceFromJSON(str)
+	buffer = make([]byte, 0)
+	_, err = CreateResourceFromJSON(buffer)
 	if err == nil {
-		t.Errorf("CreateResourceFromJSON should return an error for zero-length string")
+		t.Errorf("CreateResourceFromJSON should return an error for zero-length slice")
 	}
+
+	buffer = nil
+	_, err = CreateResourceFromJSON(buffer)
+	if err == nil {
+		t.Errorf("CreateResourceFromJSON should return an error for nil slice")
+	}
+}
+
+func TestLoadResourcesFrom(t *testing.T) {
+	var buf bytes.Buffer
+
+	path := "/tmp/testfile"
+
+	buf.WriteString("[{ \"name\": \"cats.jpg\", \"description\": \"cat picture of cats\", ")
+	buf.WriteString("\"location\": \"/tmp/somepath\", \"mimetype\": \"image/jpeg\"}]")
+
+	ioutil.WriteFile(path, buf.Bytes(), 0644)
+
+	_, err := LoadResourcesFrom(path)
+	if err != nil {
+		t.Errorf("Error loading resources from file: %v", err)
+	}
+
+	os.Remove(path) //clean up the test file
+
+}
+
+func TestAssignRandomIDs(t *testing.T) {
+	var buf bytes.Buffer
+	logrus.SetLevel(logrus.DebugLevel)
+
+	path := "/tmp/testfile"
+
+	buf.WriteString("[{ \"name\": \"cats.jpg\", \"description\": \"cat picture of cats\", ")
+	buf.WriteString("\"location\": \"/tmp/somepath\", \"mimetype\": \"image/jpeg\"}]")
+
+	ioutil.WriteFile(path, buf.Bytes(), 0644)
+
+	slice, err := LoadResourcesFrom(path)
+	if err != nil {
+		t.Errorf("Error loading resources from file: %v", err)
+	}
+
+	os.Remove(path) //clean up the test file
+
+	err = AssignRandomIDs(slice)
+	if err != nil {
+		t.Errorf("Error assigning random IDs: %v", err)
+	}
+
+	if slice[0].resourceID == nil {
+		t.Error("Resource ID was not set")
+	}
+
 }
